ab239d4172bfc035d74220574b4d66c9e0ee6dba
linh626688 <linh626688@gmail.com>
2023-09-17 22:07:24 +0700

    cluster name

--- .circleci/deploy/deploy.sh
+++ .circleci/deploy/deploy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # Integrate kubectl with aws eks
-aws eks update-kubeconfig --region us-east-1 --name helloworld-cluster
+aws eks update-kubeconfig --region us-east-1 --name helloworld-capstone-cluster
 export KUBECONFIG=~/.kube/config
 
 # Deploy
 kubectl apply -f deployment.yml
-
-# this wait for 'Available=True' only works for initial deployment, not rolling
-kubectl wait deployment helloworld-deployment --for condition=Available=True --timeout=90s
